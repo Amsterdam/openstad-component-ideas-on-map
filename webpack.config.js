f8b3b174be8e72805b7842e42379e7b49a8ec2eb
Niels Vegter <niels@denes.nl>
2020-04-06 14:09:19 +0100

    Optimize builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
 
-	devtool: 'eval-source-map',
-	//mode: 'production',
-	mode: 'development',
+	devtool: 'source-map',
 
 	entry: './src/index.jsx',
 
@@ -25,6 +24,25 @@ module.exports = {
       ignoreOrder: false,
     }),
   ],
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        test: /\.jsx?$/,
+        exclude: /\/core-js/,
+        minify(file, sourceMap) {
+          const extractedComments = [];
+          const { error, map, code, warnings } = require('uglify-js') // Or require('./path/to/uglify-module')
+                .minify(
+                  file,
+                  { /* Your options for minification */ },
+                );
+          return { error, map, code, warnings, extractedComments };
+        }
+        
+      })
+    ]
+  },
 
 	module: {
 		rules: [
